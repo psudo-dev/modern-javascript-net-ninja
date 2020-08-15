bc5bf0cba497829616660249cfa614b59be65092
Renato Setoue <eu.recebo.spam@gmail.com>
2020-08-15 18:19:11 -0300

    reversed to original
    code before my tests

--- quiz/app.js
+++ quiz/app.js
@@ -29,38 +29,3 @@ form.addEventListener("submit", e => {
         }
     }, 10);
 })
-
-const question = "5. What's your name?"
-const qn = "q5"
-const vA = "A"
-const vB = "B"
-const altA = "Who's your daddy?"
-const altB = "Say my name!"
-const newDiv = document.createElement("div")
-newDiv.innerHTML = `<div class="my-5">
-<p class="lead font-weight-normal">${question}</p>
-<div class="form-check my-2 text-white-50">
-    <input type="radio" name="${qn}" id="" value="${vA}" checked>
-    <label for="" class="form-check-label">${altA}</label>
-</div>
-<div class="form-check my-2 text-white-50">
-    <input type="radio" name="${qn}" id="" value="${vB}">
-    <label for="" class="form-check-label">${altB}</label>
-</div>
-</div>`
-const submitButton = document.querySelector(".submit-button")
-form.insertBefore(newDiv, submitButton)
-
-const quiz = {
-    question: "",
-    alternatives: {},
-    position: index+1,
-    correct: []
-}
-
-`${q}${position}`
-`${quiz.alternatives.A}`
-`${quiz.alternatives.B}`
-`${quiz.alternatives.C}`
-`${quiz.alternatives.D}`
-`${quiz.alternatives.E}`
